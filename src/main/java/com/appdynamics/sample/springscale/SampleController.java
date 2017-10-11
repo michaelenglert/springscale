ca072907b00d7b134b6d7088dd3f87be99fbe49e
michael.englert <michi.eng@gmail.com>
2017-10-10 19:19:39 -0700

    changed timeout strategy

--- src/main/java/com/appdynamics/sample/springscale/SampleController.java
+++ src/main/java/com/appdynamics/sample/springscale/SampleController.java
@@ -6,29 +6,28 @@ import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Random;
 
 import static java.lang.Thread.sleep;
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
+    static int timeout;
 
     @RequestMapping("/")
     @ResponseBody
     String home() throws InterruptedException {
         Map<String, String> env = System.getenv();
-        for (String envName : env.keySet()) {
-            if(envName.equals("TIMEOUT")){
-                System.out.format("%s=%s%n",
-                        envName,
-                        env.get(envName));
-                sleep(Integer.parseInt(env.get(envName)));
-            }
+        if(timeout == 2){
+            sleep(10000);
         }
         return "Response!";
     }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SampleController.class, args);
+        Random rnd = null;
+        timeout =  rnd.nextInt(5) + 1;
     }
 }
